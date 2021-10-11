3267ef968ac286a9436e89cc02e5a1df4c0f9d9a
Dan Nguyen <39757208+dannguyen216@users.noreply.github.com>
2021-10-10 19:53:44 -0700

    Cleaning up SQL Exploration
    Removing commented out SQL code along with some unnecessary queries written as I was looking at the dataset.

--- formula_one/exploring_f1_data.sql
+++ formula_one/exploring_f1_data.sql
@@ -2,7 +2,14 @@
 Dataset obtained from https://www.kaggle.com/rohanrao/formula-1-world-championship-1950-2020
 */
 
--- Let's actually pay attention to the data we have and get an average lap time for each driver (per race) by averaging the 
+-- Let's take a look at the races that have happened this year so far.
+-- The date <= GETDATE() conditional is not too useful with this specific dataset
+-- because it seems like the data is limited to the first nine races of the season.
+SELECT raceId, year, round, circuitId, name, date FROM formula_one.dbo.races
+WHERE year = 2021 AND date <= GETDATE()
+ORDER BY raceId ASC;
+
+-- Let's get an average lap time for each driver (per race) by averaging the 
 -- integer millisecond values and converting the type to time
 -- REFERENCES: https://www.w3schools.com/sqL/func_sqlserver_dateadd.asp, 
 -- https://www.sqlservercentral.com/forums/topic/converting-milliseconds
@@ -12,14 +19,12 @@ FROM formula_one..lap_times as l
 GROUP BY l.raceId, l.driverId
 ORDER BY l.raceId, average_lap_time DESC;
 
-
--- Let's try to combine everything
--- REFERENCE FOR TABLEAU WOES: https://onenumber.biz/blog-1/2019/10/10/how-to-handle-milliseconds-in-tableau
+-- Let's try to combine different tables to get the average lap time, among other information, for all drivers
+-- in every race this season
 SELECT races.raceId, races.name as race_name, results.driverId, 
 CONCAT(drivers.forename, ' ', drivers.surname) as driver_name, results.number as driver_number, 
 results.position, av.average_lap_time, av.average_lap_time_ms,
 TRY_CONVERT(time, CONCAT('00:', results.fastestLapTime)) as fastest_lap_time, 
---results.fastestLapTime as fastest_lap_time, 
 av.total_laps_completed, constructors.name as team_name
 FROM formula_one.dbo.results as results 
 	JOIN formula_one.dbo.races as races
@@ -31,7 +36,7 @@ FROM formula_one.dbo.results as results
 			FROM formula_one..lap_times as l
 			GROUP BY l.raceId, l.driverId
 		) as av ON av.raceId = results.raceId AND results.driverId = av.driverId
-WHERE races.year = 2021 AND races.raceId=1057
+WHERE races.year = 2021
 ORDER BY results.raceId, av.average_lap_time;
 
 -- Let's try to get the average finishing position for drivers
@@ -41,26 +46,10 @@ GROUP BY r.driverId;
 
 -- Azerbaijan GP 2021: The interesting thing about this one is because of the red flag after Verstappen's crash near the end.
 -- This affects the lap time, which seems to include the time spent when the race was stopped into the calculation. This
--- creates outliers because Verstappen's race was ended early, so his average lap time is not affected by the stopped session
-/*
+-- creates outliers because Verstappen's race was ended early, so his average lap time is not affected by the stopped session.
+-- This query gets Lewis Hamilton's lap times in the race. Note that his lap time for lap 49 is extremely high.
 SELECT * FROM formula_one..lap_times
 WHERE raceId=1057 AND driverId=1;
-*/
-
--- This one's interesting because we can try and determine whether the driver got a DNF
--- Not sure where to go with this one. Is a DNS or DQ counted as a DNF? Is there a better term to use (e.g. Leclerc Monaco 2021)?
--- Reference link: https://stackoverflow.com/questions/5435786/aggregate-function-checking-if-contains
-SELECT r.driverId, MIN(CAST(r.positionOrder as INT)) as best_result, MAX(CAST(r.positionOrder as INT)) as worst_result,
-MAX(
-	CASE
-		WHEN r.position = '\N' THEN 1
-		ELSE 0
-	END
-) AS DNF
-FROM formula_one.dbo.results as r
-WHERE raceId >=1052
-GROUP BY r.driverId
-ORDER BY /*best_result, */r.driverId;
 
 -- Checking for null values for race position in the results table
 -- Seems to correspond to drivers not finishing the race
@@ -69,6 +58,20 @@ FROM formula_one.dbo.results as r
 WHERE r.position = '\N'
 ORDER BY r.driverId;
 
+-- Last race with results for each year
+SELECT ra.year, ra.raceId, ra.name, ra.date AS last_race_date
+FROM formula_one.dbo.races as ra
+WHERE ra.date in (
+	SELECT MAX(ra2.date) 
+	FROM formula_one.dbo.races as ra2 JOIN formula_one.dbo.results as re
+	ON ra2.raceId = re.raceId
+	WHERE ra2.year >= 2014
+	GROUP BY ra2.year
+)
+
+/*
+Below are queries used to explore the McLaren F1 team's performance from 2014-2021
+*/
 -- Looking at McLaren's development during the F1 Hybrid Era (2014 - Present)
 SELECT * FROM formula_one.dbo.results as re WHERE constructorId = 1 AND raceId >=900 ORDER BY CAST(raceId as INT);
 
@@ -80,11 +83,6 @@ ORDER BY raceId;
 SELECT * FROM formula_one.dbo.constructor_standings as cs WHERE constructorId = 1 AND raceId >= 900
 ORDER BY raceId;
 
-SELECT * FROM formula_one.dbo.lap_times as l WHERE raceId >= 900
-ORDER BY l.raceId;
-
-SELECT * FROM formula_one.dbo.races as ra WHERE year >= 2014 ORDER BY CAST(raceId as INT);
-
 -- McLaren average race results
 SELECT ra.year, AVG(CAST(re.positionOrder AS FLOAT)) as avg_race_result, AVG(CAST(re.points AS FLOAT)) as avg_points_scored
 FROM formula_one.dbo.races as ra JOIN formula_one.dbo.results as re
@@ -93,27 +91,13 @@ WHERE ra.year >= 2014 AND constructorId = 1
 GROUP BY ra.year
 ORDER BY ra.year;
 
--- McLaren race results in the hybrid era (2014 - Present)
+-- All McLaren race results in the hybrid era (2014 - Present)
 SELECT ra.year, re.raceId, re.driverId, re.positionOrder as race_result, re.points
 FROM formula_one.dbo.results as re JOIN formula_one.dbo.races as ra
 ON re.raceId = ra.raceId
 WHERE ra.year >= 2014 AND constructorId = 1;
 
--- Last race with results for each year
-SELECT ra.year, ra.raceId, ra.name, ra.date AS last_race_date, co.constructorId, co.position as constructor_standing
-FROM formula_one.dbo.races as ra JOIN formula_one.dbo.constructor_standings as co
-ON ra.raceId = co.raceId
-WHERE co.constructorId = 1 AND ra.date in (
-	SELECT MAX(ra2.date) 
-	FROM formula_one.dbo.races as ra2 JOIN formula_one.dbo.results as re
-	ON ra2.raceId = re.raceId
-	WHERE ra2.year >= 2014
-	GROUP BY ra2.year
-)
-
 -- Number of McLaren podium results
--- An absolute disaster of a query this is
--- Link for reference: https://www.codeproject.com/Questions/5274830/How-to-display-result-when-countequals0-for-SQL-in
 SELECT listYears.year, COUNT(numPodium.raceId) as number_of_podiums
 FROM
 (
@@ -123,16 +107,15 @@ FROM
 ) as listYears
 LEFT JOIN
 (
-	SELECT ra2.year, re.raceId--COUNT(re.raceId)
+	SELECT ra2.year, re.raceId
 	FROM formula_one.dbo.results as re RIGHT JOIN formula_one.dbo.races as ra2
 	ON re.raceId = ra2.raceId
 	WHERE ra2.year >= 2014 AND constructorId = 1 AND CAST(re.positionOrder as INT) <= 3
-	--GROUP BY ra2.year
 ) as numPodium
 ON listYears.year = numPodium.year
 GROUP BY listYears.year
 
--- Average lap time, total
+-- Average lap time for all drivers in each race (on or after 2014)
 SELECT l.raceId,  CONVERT(time, DATEADD(ms, AVG(CAST(l.milliseconds as float)), 0)) as average_lap_time,
 AVG(CAST(l.milliseconds as float)) as 'average_lap_time (ms)'
 FROM formula_one..lap_times as l JOIN formula_one..results as r
@@ -142,35 +125,16 @@ WHERE ra.year >= 2014
 GROUP BY l.raceId
 ORDER BY l.raceId, average_lap_time DESC;
 
--- Average lap time for McLaren (2014 - 2021) for real
+-- Average lap time in each race for McLaren (2014 - 2021)
 SELECT l.raceId, r.constructorId, CONVERT(time, DATEADD(ms, AVG(CAST(l.milliseconds as float)), 0)) as mclaren_average_lap_time,
 AVG(CAST(l.milliseconds AS decimal)) as mclaren_average_lap_time_ms
 FROM formula_one..lap_times as l JOIN formula_one..results as r
 ON l.raceId = r.raceId AND r.driverId = l.driverId
---AND r.constructorId = 1
---WHERE l.raceId = 900 AND r.constructorId = 1
 JOIN formula_one..races as ra
 ON l.raceId = ra.raceId AND r.raceId = ra.raceId
---AND r.constructorId = 1
 WHERE ra.year >= 2014 AND constructorId = 1
 GROUP BY l.raceId, r.constructorId
 ORDER BY l.raceId
-
--- Checking McLaren Results just in case
-SELECT * FROM formula_one..lap_times as l join formula_one..results as r
-ON l.raceId = r.raceId AND r.driverId = l.driverId
-WHERE r.constructorId = 1 AND r.raceId = 1060;
-
-SELECT * FROM formula_one..races as r JOIN formula_one..results as re
-ON r.raceId = re.raceId
-WHERE r.year >= 2014;
-
--- Position changes according to value for each lap - Worth looking into
-SELECT raceId, driverId, COUNT(DISTINCT position) AS position_changes
-FROM formula_one.dbo.results
-WHERE raceId >= 900
-GROUP BY raceId, driverId
-ORDER BY raceId;
 
 -- Last time McLaren won a race
 SELECT TOP 1 re.raceId, ra.year, ra.name, re.driverId, re.positionOrder
@@ -180,7 +144,7 @@ ON re.raceId = ra.raceId
 WHERE re.constructorId = 1 AND re.positionOrder = '1'
 ORDER BY raceId DESC;
 
--- Last race each year in the hybrid era - displays year, raceId, constructor standings/points/wins, date
+-- Last race each year in the hybrid era - displays year, raceId, McLaren standings/points/wins, date
 SELECT r.year, r.raceId, c.position, c.points, c.wins, r.date
 FROM formula_one.dbo.constructor_standings as c
 JOIN formula_one.dbo.races as r ON c.raceId = r.raceId
